57c38e09c6f5874b336a9ccf80f9eadc3b67ca58
Hod Sinay <sinayhod@gmail.com>
2019-05-03 13:48:25 +0300

    added auto git pull

--- CreateLesson.cmd
+++ CreateLesson.cmd
@@ -1,4 +1,5 @@
 @echo off
+git pull origin master > NUL 2>&1
 cd "C:\Program Files\HasulamLessonNameManager"
 "C:\Python27\python.exe" "C:\Program Files\HasulamLessonNameManager\OpenWME.py"
 cd "C:\Users\Administrator\Desktop\HodsRecorder"
